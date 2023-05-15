9bba7dc70920abef742f24822cbabf6ef836b67a
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-15 18:15:18 +0200

    Add react-bootstrap and navbar skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import "./api/axiosDefaults";
+import axios from "axios";
 import GoogleCalendarAPI from "./components/GoogleCalendarAPI";
 import DemoApp from "./components/Calendar";
+import { useEffect, useState } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       {/* <GoogleCalendarAPI /> */}
-      <DemoApp />
+      {/* <DemoApp /> */}
     </div>
   );
 }
