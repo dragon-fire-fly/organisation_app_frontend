ab6e6349d888f6c39fc70d808c44d823d76a2379
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-15 15:36:27 +0200

    Add google calendar API event creation

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { useState } from "react";
 function App() {
   const [start, setStart] = useState(new Date());
   const [end, setEnd] = useState(new Date());
+  const [eventName, setEventName] = useState("");
+  const [eventDescription, seteventDescription] = useState("");
 
   const session = useSession(); // tokens: when session exists, we have a user!
   const supabase = useSupabaseClient(); // talk to supabase!
@@ -39,8 +41,44 @@ function App() {
     await supabase.auth.signOut();
   }
 
+  async function createCalendarEvent() {
+    console.log("Creating calendar event");
+    const event = {
+      summary: eventName,
+      description: eventDescription,
+      start: {
+        dateTime: start.toISOString(),
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      },
+      end: {
+        dateTime: end.toISOString(),
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      },
+    };
+    // calendars/calendarId/events - primary selects the primary calendar of the logged in user
+    await fetch(
+      "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + session.provider_token, //Access token for google
+        },
+        body: JSON.stringify(event),
+      }
+    )
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        alert("Event created, check your Google calendar!");
+      });
+  }
+
   console.log(session);
   console.log(start);
+  console.log(eventName);
+  console.log(eventDescription);
 
   return (
     <div className="App">
@@ -52,6 +90,21 @@ function App() {
             <DateTimePicker onChange={setStart} value={start} />
             <p>End of your event</p>
             <DateTimePicker onChange={setEnd} value={end} />
+            <p>Event Name</p>
+            <input
+              type="text"
+              onChange={(e) => setEventName(e.target.value)}
+            />
+            <p>Event Description</p>
+            <input
+              type="text"
+              onChange={(e) => seteventDescription(e.target.value)}
+            />
+            <hr />
+            <button onClick={() => createCalendarEvent()}>
+              Create calendar event
+            </button>
+            <br />
             <button onClick={() => googleSignOut()}>Sign out</button>
           </>
         ) : (
