51892a79fdf0fc6e926ca060ba973d501832adac
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-15 15:17:59 +0200

    Add supabase context provider & datetime pickers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  useSession,
+  useSupabaseClient,
+  useSessionContext,
+} from "@supabase/auth-helpers-react";
+import DateTimePicker from "react-datetime-picker";
+import { useState } from "react";
 
 function App() {
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
+
+  const session = useSession(); // tokens: when session exists, we have a user!
+  const supabase = useSupabaseClient(); // talk to supabase!
+  const { isLoading } = useSessionContext();
+
+  // to prevent screen refresh on reloading
+  if (isLoading) {
+    return <></>;
+  }
+
+  async function googleSignIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        scopes:
+          "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events",
+      },
+    });
+    if (error) {
+      alert("Error logging in to Google provider with Supabase");
+      console.log(error);
+    }
+  }
+
+  async function googleSignOut() {
+    await supabase.auth.signOut();
+  }
+
+  console.log(session);
+  console.log(start);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "400px", margin: "30px auto" }}>
+        {session ? (
+          <>
+            <h2>Hey there {session.user.email}</h2>
+            <p>Start of your event</p>
+            <DateTimePicker onChange={setStart} value={start} />
+            <p>End of your event</p>
+            <DateTimePicker onChange={setEnd} value={end} />
+            <button onClick={() => googleSignOut()}>Sign out</button>
+          </>
+        ) : (
+          <>
+            <button onClick={() => googleSignIn()}>Sign in with Google</button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createClient } from "@supabase/supabase-js";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+const supabase = createClient(
+  "https://kckvhjrrosiqgfccjstt.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtja3ZoanJyb3NpcWdmY2Nqc3R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxNDQ2NDMsImV4cCI6MTk5OTcyMDY0M30.vH55p7swuOFz3w6cMMO6NtcB_3-MCDbJGVLwkdk3vh8"
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
