e8790221c3c61e3cbe8384c49c908bad2b4796d0
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-16 09:26:22 +0200

    Refactor user logged in context code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { CurrentUserContext } from "../App";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext);
+  const currentUser = useCurrentUser();
 
   const loggedInIcons = <>{currentUser?.username}</>;
   const loggedOutIcons = (
@@ -27,7 +27,6 @@ const NavBar = () => {
       </NavLink>
     </>
   );
-
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -36,7 +35,6 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -48,7 +46,6 @@ const NavBar = () => {
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
-
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
@@ -56,5 +53,4 @@ const NavBar = () => {
     </Navbar>
   );
 };
-
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,26 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createClient } from "@supabase/supabase-js";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
-
-const supabase = createClient(
-  "https://kckvhjrrosiqgfccjstt.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtja3ZoanJyb3NpcWdmY2Nqc3R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxNDQ2NDMsImV4cCI6MTk5OTcyMDY0M30.vH55p7swuOFz3w6cMMO6NtcB_3-MCDbJGVLwkdk3vh8"
-);
+import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <SessionContextProvider supabaseClient={supabase}>
-      <Router>
+    <Router>
+      <CurrentUserProvider>
         <App />
-      </Router>
-    </SessionContextProvider>
+      </CurrentUserProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
