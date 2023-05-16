bdb8dd27443e5242e71c75d3dba35ac202274916
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-16 21:23:33 +0200

    Add handleFollow async func for dynamic updates

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,20 +1,62 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { axiosReq } from "../api/axiosDefaults";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "./CurrentUserContext";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
-
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
-
 export const ProfileDataProvider = ({ children }) => {
   const [profileData, setProfileData] = useState({
     // we will use the pageProfile later!
     pageProfile: { results: [] },
     popularProfiles: { results: [] },
   });
+
   const currentUser = useCurrentUser();
+
+  const handleFollow = async (clickedProfile) => {
+    try {
+      const { data } = await axiosRes.post("/followers/", {
+        followed: clickedProfile.id,
+      });
+
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) => {
+            return profile.id === clickedProfile.id
+              ? // if the profile id is the same as the one just clicked on, the followers count is increased by 1
+                {
+                  ...profile, // spread the profile
+                  followers_count: profile.followers_count + 1, // and add one to the followers count
+                  following_id: data.id, // and set the following_id to data.id
+                }
+              : profile.is_owner // check if the profile in the array we're iterating over is owner by the currently logged in user
+              ? { ...profile, following_count: profile.following_count + 1 } // if so, increase that profile's following count by one
+              : profile; // return the profile without changing it
+          }),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) => {
+            return profile.id === clickedProfile.id
+              ? // if the profile id is the same as the one just clicked on, the followers count is increased by 1
+                {
+                  ...profile, // spread the profile
+                  followers_count: profile.followers_count + 1, // and add one to the followers count
+                  following_id: data.id, // and set the following_id to data.id
+                }
+              : profile.is_owner // check if the profile in the array we're iterating over is owner by the currently logged in user
+              ? { ...profile, following_count: profile.following_count + 1 } // if so, increase that profile's following count by one
+              : profile; // return the profile without changing it
+          }),
+        },
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     const handleMount = async () => {
@@ -30,13 +72,12 @@ export const ProfileDataProvider = ({ children }) => {
         console.log(err);
       }
     };
-
     handleMount();
   }, [currentUser]);
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={setProfileData}>
+      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
