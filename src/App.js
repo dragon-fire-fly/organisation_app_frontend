b89d459bfe6b2129fba6e185e49bb42847dbe879
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-16 21:20:51 +0200

    Add profile page component and style

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -59,6 +60,7 @@ function App() {
             render={() => <PostEditForm />}
           />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
 
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
