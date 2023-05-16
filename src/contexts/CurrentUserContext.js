3c9f1f92b7b2ba6917561b57906be3caddc34e6f
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-16 14:29:44 +0200

    Add auth token interceptors

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,27 +1,80 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import axios from "axios";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
+import { response } from "msw";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
-
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
 
   const handleMount = async () => {
     try {
-      const { data } = await axios.get("dj-rest-auth/user/");
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
     }
   };
-
   useEffect(() => {
     handleMount();
   }, []);
+
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history.push("/signin");
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+
+    axiosRes.interceptors.response.use(
+      (repsonse) => response,
+      async (err) => {
+        // if we get a 401 error...
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/"); // try to refresh the token
+            // if we can't refresh the token
+          } catch (err) {
+            // check for previously logged in state
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin"); // and if present, redirect to sign in page
+              }
+              return null; // and set their data to null
+            });
+          } // if theres no error refreshing the token, return axios instance with error config to exit interceptor
+          return axios(err.config);
+        }
+        return Promise.reject(err); // if the error wasn't 401, reject the Promise with error and exit interceptor
+      }
+    );
+  }, [history]);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
