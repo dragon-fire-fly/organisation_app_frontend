05df0d4daa8cfafcfd826b29bc5739776d8ff97e
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-22 11:41:19 +0200

    Add Event component with basic event details

---
+++ src/pages/events/Event.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from "react-bootstrap";
+import Avatar from "../../components/Avatar";
+
+const Event = (props) => {
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    title,
+    content,
+    image,
+    event_type,
+    location,
+    start_at,
+    end_at,
+    // all_day,
+    privacy,
+  } = props;
+
+  console.log(props);
+
+  const currentUser = useCurrentUser();
+
+  const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  return (
+    <Card>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+        </Media>
+      </Card.Body>
+      <Link to={`/events/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+        {event_type && <Card.Text>{event_type}</Card.Text>}
+        {start_at && (
+          <Card.Text>
+            {start_at} - {end_at}
+          </Card.Text>
+        )}
+        {location && <Card.Text>{location}</Card.Text>}
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Event;
