469f68a31df5f99322e151a854ae8753100d0126
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-22 12:12:19 +0200

    Add events to events page with infinite scroll

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -1,22 +1,89 @@
-import React, { useState } from "react";
-import { Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom/cjs/react-router-dom.min";
-// import styles from "../../styles/EventsPage.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/EventsPage.module.css";
+import PopularProfiles from "../profiles/PopularProfiles";
+import InfiniteScroll from "react-infinite-scroll-component";
+import NoResults from "../../assets/no-results.png";
+import Asset from "../../components/Asset";
+import Event from "./Event";
+import { fetchMoreData } from "../../utils/utils";
 
-function EventsPage() {
+function EventsPage({ message, filter = "" }) {
   const [events, setEvents] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   // to detect url changes
   const { pathname } = useLocation();
   const [query, setQuery] = useState("");
 
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          `/events/?${filter}search=${query}`
+        );
+        setEvents(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchEvents();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
+
   return (
-    <>
-      <Row className="h-100">
-        <h1>Events Page</h1>
-      </Row>
-      <Row></Row>
-    </>
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search Events"
+          />
+        </Form>
+        {hasLoaded ? (
+          <>
+            {events.results.length ? (
+              <InfiniteScroll
+                children={events.results.map((event) => (
+                  <Event key={event.id} {...event} setEvents={setEvents} />
+                ))}
+                dataLength={events.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!events.next}
+                next={() => fetchMoreData(events, setEvents)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
   );
 }
 
