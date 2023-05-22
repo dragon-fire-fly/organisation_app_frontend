90615a001dd0167b7f33c88d60ba97f7cd7fb47d
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-22 12:11:08 +0200

    Add EventPage component

---
+++ src/pages/events/EventPage.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import PopularProfiles from "../profiles/PopularProfiles";
+import Event from "./Event";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosReq } from "../../api/axiosDefaults";
+
+function EventPage() {
+  const { id } = useParams();
+  const [event, setEvent] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: event }] = await Promise.all([
+          axiosReq.get(`/events/${id}`),
+        ]);
+        setEvent({ results: [event] });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <Event {...event.results[0]} setEvents={setEvent} eventPage />
+        <Container className={appStyles.Content}>Memories</Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
+  );
+}
+
+export default EventPage;
