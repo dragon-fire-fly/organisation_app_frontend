1db9a71bc445c72f7fd1d283b9b9840ee33d74bc
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-24 00:17:28 +0200

    Conditionally render event & date modals with btns

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -1,38 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const EventModal = (props) => {
   // console.log(props);
-  const { show, setShow, handleClose, header, body } = props;
+  const { show, setShow, handleClose, header, body, specificEvent } = props;
 
-  console.log(body);
+  console.log(specificEvent);
+
+  const addEventRedirect = () => console.log("button pressed");
 
   return (
     // <Modal show={show} onHide={() => setShow(false)}>
     <Modal show={show}>
       <Modal.Header>{header}</Modal.Header>
       <Modal.Body>
-        {typeof body == "object" ? (
-          body[0]?.length ? (
-            body[0].map((event) => (
-              <>
-                <p key={event.title}>{event.title}</p>
-                <p key={event.id}>
-                  from {event.start} 'til {event.end}
-                </p>
-              </>
-            ))
-          ) : (
-            <p>No events scheduled today</p>
-          )
+        {specificEvent ? (
+          <>
+            <p key={body[0][0].title}>{body[0][0].title}</p>
+            <p key={body[0][0].id}>
+              from {body[0][0].start} 'til {body[0][0].end}
+            </p>
+          </>
+        ) : body[0]?.length ? (
+          body[0].map((event) => (
+            <>
+              <Link
+                // to={{ pathname: "/events", state: { event } }}
+                key={event.title}
+              >
+                {event.title}
+              </Link>
+              <p key={event.id}>
+                from {event.start} 'til {event.end}
+              </p>
+            </>
+          ))
         ) : (
-          console.log("figuring it out")
+          <p>No events scheduled today</p>
         )}
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={handleClose}>cancel</Button>
 
-        <Button>submit</Button>
+        {specificEvent ? (
+          <Button>view event</Button>
+        ) : (
+          <Button onClick={addEventRedirect}>add event</Button>
+        )}
       </Modal.Footer>
     </Modal>
   );
