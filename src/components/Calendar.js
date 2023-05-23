0d2debdf699a6715146ed8613af3a9aacd847199
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-23 10:01:25 +0200

    Fetch events from API for displaying in calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,23 +6,20 @@ import listPlugin from "@fullcalendar/list";
 import multiMonthPlugin from "@fullcalendar/multimonth";
 import interactionPlugin from "@fullcalendar/interaction";
 
-export default class EventCalendar extends React.Component {
-  render(props) {
-    return (
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin]}
-        initialView="dayGridMonth"
-        headerToolbar={{
-          start: "today prev,next",
-          center: "title",
-          end: "multiMonthYear,dayGridMonth,timeGridWeek,timeGridDay",
-        }}
-        height={"90vh"}
-        events={[
-          { title: "event 1", date: "2023-05-01" },
-          { title: "event 2", date: "2023-05-18" },
-        ]}
-      />
-    );
-  }
+function EventCalendar(props) {
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin]}
+      initialView="dayGridMonth"
+      headerToolbar={{
+        start: "today prevYear,prev,next,nextYear",
+        center: "title",
+        end: "multiMonthYear,dayGridMonth,timeGridWeek,timeGridDay",
+      }}
+      height={"90vh"}
+      events={props.events}
+    />
+  );
 }
+
+export default EventCalendar;
--- src/pages/calendar/CalendarPage.js
+++ src/pages/calendar/CalendarPage.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EventCalendar from "../../components/Calendar";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
-function CalendarPage() {
-  return <EventCalendar />;
+function CalendarPage(filter = "") {
+  const [events, setEvents] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          `/events/calendar`
+          // `/events/?${filter}search=${query}`
+        );
+        setEvents(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchEvents();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
+
+  console.log(events?.results);
+
+  const testEvents = [
+    {
+      title: "event 1",
+      start: "2023-05-01 17:00:00",
+      end: "2023-05-03 19:00:00",
+    },
+    { title: "event 2", date: "2023-05-18" },
+  ];
+  return <EventCalendar events={events?.results} />;
 }
 
 export default CalendarPage;
