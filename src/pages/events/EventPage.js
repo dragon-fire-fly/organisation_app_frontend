2dd8c0e4bbd2ca0d6021fe0d5a6b0981a7cd3130
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-24 22:39:46 +0200

    Add loading spinners to individual post & event pg

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -7,6 +7,9 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import PopularProfiles from "../profiles/PopularProfiles";
+import Asset from "../../components/Asset";
+import InfiniteScroll from "react-infinite-scroll-component";
+import CommentCreateForm from "../comments/CommentCreateForm";
 import Event from "./Event";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -14,6 +17,10 @@ import { axiosReq } from "../../api/axiosDefaults";
 function EventPage() {
   const { id } = useParams();
   const [event, setEvent] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
 
   useEffect(() => {
     const handleMount = async () => {
@@ -22,24 +29,33 @@ function EventPage() {
           axiosReq.get(`/events/${id}`),
         ]);
         setEvent({ results: [event] });
+        setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
     };
-
+    setHasLoaded(false);
     handleMount();
   }, [id]);
 
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <PopularProfiles mobile />
-        <Event {...event.results[0]} setEvents={setEvent} eventPage />
-        <Container className={appStyles.Content}>Memories</Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        <PopularProfiles />
-      </Col>
+      {hasLoaded ? (
+        <>
+          <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <PopularProfiles mobile />
+            <Event {...event.results[0]} setEvents={setEvent} eventPage />
+            <Container className={appStyles.Content}>Memories</Container>
+          </Col>
+          <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+            <PopularProfiles />
+          </Col>
+        </>
+      ) : (
+        <Container className={appStyles.Content}>
+          <Asset spinner />
+        </Container>
+      )}
     </Row>
   );
 }
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -20,6 +20,7 @@ function PostPage() {
   const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
   const [comments, setComments] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
   useEffect(() => {
     const handleMount = async () => {
       try {
@@ -29,54 +30,64 @@ function PostPage() {
         ]);
         setPost({ results: [post] });
         setComments(comments);
+        setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
     };
+    setHasLoaded(false);
     handleMount();
   }, [id]);
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <PopularProfiles mobile />
-        <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>
-          {currentUser ? (
-            <CommentCreateForm
-              profile_id={currentUser.profile_id}
-              profileImage={profile_image}
-              post={id}
-              setPost={setPost}
-              setComments={setComments}
-            />
-          ) : comments.results.length ? (
-            "Comments"
-          ) : null}
-          {comments.results.length ? (
-            <InfiniteScroll
-              children={comments.results.map((comment) => (
-                <Comment
-                  key={comment.id}
-                  {...comment}
+      {hasLoaded ? (
+        <>
+          <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <PopularProfiles mobile />
+            <Post {...post.results[0]} setPosts={setPost} postPage />
+            <Container className={appStyles.Content}>
+              {currentUser ? (
+                <CommentCreateForm
+                  profile_id={currentUser.profile_id}
+                  profileImage={profile_image}
+                  post={id}
                   setPost={setPost}
                   setComments={setComments}
                 />
-              ))}
-              dataLength={comments.results.length}
-              loader={<Asset spinner />}
-              hasMore={!!comments.next}
-              next={() => fetchMoreData(comments, setComments)}
-            />
-          ) : currentUser ? (
-            <span>No comments yet, be the first to comment!</span>
-          ) : (
-            <span>No comments... yet</span>
-          )}
+              ) : comments.results.length ? (
+                "Comments"
+              ) : null}
+              {comments.results.length ? (
+                <InfiniteScroll
+                  children={comments.results.map((comment) => (
+                    <Comment
+                      key={comment.id}
+                      {...comment}
+                      setPost={setPost}
+                      setComments={setComments}
+                    />
+                  ))}
+                  dataLength={comments.results.length}
+                  loader={<Asset spinner />}
+                  hasMore={!!comments.next}
+                  next={() => fetchMoreData(comments, setComments)}
+                />
+              ) : currentUser ? (
+                <span>No comments yet, be the first to comment!</span>
+              ) : (
+                <span>No comments... yet</span>
+              )}
+            </Container>
+          </Col>
+          <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+            <PopularProfiles />
+          </Col>
+        </>
+      ) : (
+        <Container className={appStyles.Content}>
+          <Asset spinner />
         </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        <PopularProfiles />
-      </Col>
+      )}
     </Row>
   );
 }
