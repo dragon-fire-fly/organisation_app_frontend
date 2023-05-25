0ba47c40f8a6544c7ae1eb25c074f9c79537322e
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-25 14:54:10 +0200

    Update event page to add memory & form components

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -9,10 +9,12 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import PopularProfiles from "../profiles/PopularProfiles";
 import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
-import CommentCreateForm from "../comments/CommentCreateForm";
+import MemoryCreateForm from "../memories/MemoryCreateForm";
 import Event from "./Event";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
+import Memory from "../memories/Memory";
+import { fetchMoreData } from "../../utils/utils";
 
 function EventPage() {
   const { id } = useParams();
@@ -21,14 +23,17 @@ function EventPage() {
 
   const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
+  const [memories, setMemories] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: event }] = await Promise.all([
+        const [{ data: event }, { data: memories }] = await Promise.all([
           axiosReq.get(`/events/${id}`),
+          axiosReq.get(`/memories/?event=${id}`),
         ]);
         setEvent({ results: [event] });
+        setMemories(memories);
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
@@ -45,7 +50,39 @@ function EventPage() {
           <Col className="py-2 p-0 p-lg-2" lg={8}>
             <PopularProfiles mobile />
             <Event {...event.results[0]} setEvents={setEvent} eventPage />
-            <Container className={appStyles.Content}>Memories</Container>
+            <Container className={appStyles.Content}>
+              {currentUser ? (
+                <MemoryCreateForm
+                  profile_id={currentUser.profile_id}
+                  profileImage={profile_image}
+                  event={id}
+                  setEvent={setEvent}
+                  setMemories={setMemories}
+                />
+              ) : memories.results.length ? (
+                "Memories"
+              ) : null}
+              {memories.results.length ? (
+                <InfiniteScroll
+                  children={memories.results.map((memory) => (
+                    <Memory
+                      key={memory.id}
+                      {...memory}
+                      setEvent={setEvent}
+                      setMemories={setMemories}
+                    />
+                  ))}
+                  dataLength={memories.results.length}
+                  loader={<Asset spinner />}
+                  hasMore={!!memories.next}
+                  next={() => fetchMoreData(memories, setMemories)}
+                />
+              ) : currentUser ? (
+                <span>No memories yet, be the first to add one!</span>
+              ) : (
+                <span>No memories... yet</span>
+              )}
+            </Container>
           </Col>
           <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
             <PopularProfiles />
