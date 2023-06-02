31d637dfa336098b42ed7ecd010cadc9ee0b194b
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-02 14:31:50 +0200

    Add PastEventsPage & filter on event past status

---
+++ src/pages/events/PastEventsPage.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/EventsPage.module.css";
+import PopularProfiles from "../profiles/PopularProfiles";
+import InfiniteScroll from "react-infinite-scroll-component";
+import NoResults from "../../assets/no-results.png";
+import Asset from "../../components/Asset";
+import Event from "./Event";
+import { fetchMoreData } from "../../utils/utils";
+import UpcomingEvents from "./UpcomingEvents";
+import dateFormat from "dateformat";
+
+function PastEventsPage({ message, filter = "" }) {
+  const [events, setEvents] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  // to detect url changes
+  const { pathname } = useLocation();
+  const [query, setQuery] = useState("");
+
+  const now = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          `/events/?${filter}search=${query}`
+        );
+
+        setEvents(data);
+        console.log(events);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchEvents();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <UpcomingEvents events={events} />
+      </Col>
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <UpcomingEvents events={events} mobile />
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search Events"
+          />
+        </Form>
+        {hasLoaded ? (
+          <>
+            {events.results.length ? (
+              <InfiniteScroll
+                children={events.results
+                  .filter((event) => event["past"] === true)
+                  .map((event) => (
+                    <Event key={event.id} {...event} setEvents={setEvents} />
+                  ))}
+                dataLength={events.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!events.next}
+                next={() => fetchMoreData(events, setEvents)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+    </Row>
+  );
+}
+
+export default PastEventsPage;
