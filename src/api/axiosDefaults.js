86546d21bc82bf319ba8842bacd48b2cda48c4ff
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-02 16:37:34 +0200

    Change axios base url to live site

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
-// axios.defaults.baseURL = "https://organisation-app-api.herokuapp.com/";
+// axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = "https://organisation-app-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
