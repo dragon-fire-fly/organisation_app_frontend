aff9c487781a2cced14b430ce63903ee9485bb9e
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-05 14:31:29 +0200

    Add 404 not found page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Asset from "../components/Asset";
+import styles from "../styles/NotFound.module.css";
+import NotFoundImg from "../assets/not-found.png";
+import { Button } from "react-bootstrap";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className={styles.NotFound}>
+        <div className={styles.Error404}>404!</div>
+        <div className={styles.NotFoundText}>
+          Sorry, that page couldn't be found!
+        </div>
+        <div>
+          <Asset src={NotFoundImg} />
+        </div>
+        <div className={`text-center`}>
+          <Button className={styles.Button}>
+            <Link className={styles.Link} to={"/"}>
+              Click here to return to the Homepage
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
