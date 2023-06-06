832841b8719c09df5351b55664513007884634f7
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-06 21:50:38 +0200

    Add delete confirm modal to events

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/Event.module.css";
 import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
@@ -7,6 +7,7 @@ import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { MoreDropdown } from "../../components/MoreDropdown";
 import dateFormat from "dateformat";
+import DeleteConfirmModal from "../../components/DeleteConfirmModal";
 
 const Event = (props) => {
   const {
@@ -33,8 +34,18 @@ const Event = (props) => {
 
   const currentUser = useCurrentUser();
 
+  const [show, setShow] = useState(false);
+  const [message, setMessage] = useState("");
+
   const is_owner = currentUser?.username === owner;
   const history = useHistory();
+
+  const handleShow = () => {
+    setShow(true);
+    setMessage(`Are you sure you want to delete you event "${title}"?`);
+  };
+
+  const handleClose = () => setShow(false);
 
   const handleEdit = () => {
     history.push(`/events/${id}/edit`);
@@ -144,124 +155,132 @@ const Event = (props) => {
   };
 
   return (
-    <Card>
-      <Card.Body>
-        <Media className="align-items-center justify-content-between">
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={55} />
-            {owner}
-          </Link>
-          <div className="d-flex align-items-center">
-            {/* <span>{updated_at}</span> */}
-            {is_owner && eventPage && (
-              <MoreDropdown
-                handleEdit={handleEdit}
-                handleDelete={handleDelete}
-              />
-            )}
-          </div>
-        </Media>
-      </Card.Body>
-      <Link to={`/events/${id}`}>
-        <Card.Img src={image} alt={title} />
-      </Link>
-      <Card.Body className="text-center">
-        {title && (
-          <Card.Title className={`text-center ${styles.bold}`}>
-            {title}
-          </Card.Title>
-        )}
-        {content && <Card.Text>{content}</Card.Text>}
-        {event_type && (
-          <Card.Text>
-            <u>
-              <strong>Type:</strong>
-            </u>{" "}
-            {event_type}
-          </Card.Text>
-        )}
-        {start && (
-          <Card.Text>
-            <u>
-              <strong>When:</strong>
-            </u>{" "}
-            {dateFormat(start, "mmmm dS 'yy HH:MM")}-
-            {dateFormat(end, "mmmm dS 'yy HH:MM")}
-          </Card.Text>
-        )}
-        {location && (
-          <Card.Text>
-            <u>
-              <strong>Where:</strong>
-            </u>{" "}
-            {location}
-          </Card.Text>
-        )}
+    <>
+      <Card>
+        <Card.Body>
+          <Media className="align-items-center justify-content-between">
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar src={profile_image} height={55} />
+              {owner}
+            </Link>
+            <div className="d-flex align-items-center">
+              {/* <span>{updated_at}</span> */}
+              {is_owner && eventPage && (
+                <MoreDropdown
+                  handleEdit={handleEdit}
+                  handleShow={handleShow}
+                />
+              )}
+            </div>
+          </Media>
+        </Card.Body>
+        <Link to={`/events/${id}`}>
+          <Card.Img src={image} alt={title} />
+        </Link>
+        <Card.Body className="text-center">
+          {title && (
+            <Card.Title className={`text-center ${styles.bold}`}>
+              {title}
+            </Card.Title>
+          )}
+          {content && <Card.Text>{content}</Card.Text>}
+          {event_type && (
+            <Card.Text>
+              <u>
+                <strong>Type:</strong>
+              </u>{" "}
+              {event_type}
+            </Card.Text>
+          )}
+          {start && (
+            <Card.Text>
+              <u>
+                <strong>When:</strong>
+              </u>{" "}
+              {dateFormat(start, "mmmm dS 'yy HH:MM")}-
+              {dateFormat(end, "mmmm dS 'yy HH:MM")}
+            </Card.Text>
+          )}
+          {location && (
+            <Card.Text>
+              <u>
+                <strong>Where:</strong>
+              </u>{" "}
+              {location}
+            </Card.Text>
+          )}
 
-        <div className={styles.EventBar}>
-          {is_owner ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can't watch your own Event!</Tooltip>}
-            >
-              <i className="fa-regular fa-eye" />
-            </OverlayTrigger>
-          ) : watch_id ? (
-            <span onClick={handleUnwatch}>
-              <i className={`fa-solid fa-eye ${styles.Eyes}`} />
-            </span>
-          ) : currentUser ? (
-            <span onClick={handleWatch}>
-              <i className={`fa-regular fa-eye ${styles.EyesOutline}`} />
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Log in to watch events!</Tooltip>}
-            >
-              <i className="fa-regular fa-eye" />
-            </OverlayTrigger>
-          )}
-          {watches_count}
-          <Link to={`/events/${id}`}>
-            <i className="fa-solid fa-brain" />
-          </Link>
-          {memories_count}
-          {is_owner ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={
-                <Tooltip>
-                  You cannot remove your own event from your calendar!
-                </Tooltip>
-              }
-            >
-              <i className={`fa-solid fa-calendar-check ${styles.Eyes}`} />
-            </OverlayTrigger>
-          ) : calendars.includes(currentUser?.pk) ? (
-            <span onClick={handleRemoveFromCalendar}>
-              <i className={`fa-solid fa-calendar-check ${styles.Eyes}`} />
-            </span>
-          ) : currentUser ? (
-            <span onClick={handleAddToCalendar}>
-              <i
-                className={`fa-regular fa-calendar-check ${styles.EyesOutline}`}
-              />
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={
-                <Tooltip>Log in to add events to your calendar!</Tooltip>
-              }
-            >
-              <i className="fa-regular fa-calendar-check" />
-            </OverlayTrigger>
-          )}
-          add to calendar
-        </div>
-      </Card.Body>
-    </Card>
+          <div className={styles.EventBar}>
+            {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't watch your own Event!</Tooltip>}
+              >
+                <i className="fa-regular fa-eye" />
+              </OverlayTrigger>
+            ) : watch_id ? (
+              <span onClick={handleUnwatch}>
+                <i className={`fa-solid fa-eye ${styles.Eyes}`} />
+              </span>
+            ) : currentUser ? (
+              <span onClick={handleWatch}>
+                <i className={`fa-regular fa-eye ${styles.EyesOutline}`} />
+              </span>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Log in to watch events!</Tooltip>}
+              >
+                <i className="fa-regular fa-eye" />
+              </OverlayTrigger>
+            )}
+            {watches_count}
+            <Link to={`/events/${id}`}>
+              <i className="fa-solid fa-brain" />
+            </Link>
+            {memories_count}
+            {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={
+                  <Tooltip>
+                    You cannot remove your own event from your calendar!
+                  </Tooltip>
+                }
+              >
+                <i className={`fa-solid fa-calendar-check ${styles.Eyes}`} />
+              </OverlayTrigger>
+            ) : calendars.includes(currentUser?.pk) ? (
+              <span onClick={handleRemoveFromCalendar}>
+                <i className={`fa-solid fa-calendar-check ${styles.Eyes}`} />
+              </span>
+            ) : currentUser ? (
+              <span onClick={handleAddToCalendar}>
+                <i
+                  className={`fa-regular fa-calendar-check ${styles.EyesOutline}`}
+                />
+              </span>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={
+                  <Tooltip>Log in to add events to your calendar!</Tooltip>
+                }
+              >
+                <i className="fa-regular fa-calendar-check" />
+              </OverlayTrigger>
+            )}
+            add to calendar
+          </div>
+        </Card.Body>
+      </Card>
+      <DeleteConfirmModal
+        showModal={show}
+        handleClose={handleClose}
+        handleDelete={handleDelete}
+        message={message}
+      />
+    </>
   );
 };
 
