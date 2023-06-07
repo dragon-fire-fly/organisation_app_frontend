4ff965fd8b9869ba4b4c13304aee86af3c1f47f8
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-07 14:03:02 +0200

    Add image to memory edit form & update for content

--- src/pages/memories/MemoryEditForm.js
+++ src/pages/memories/MemoryEditForm.js
@@ -1,32 +1,54 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import { axiosRes } from "../../api/axiosDefaults";
 
 import styles from "../../styles/MemoryCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Upload from "../../assets/upload.png";
+import { Image } from "react-bootstrap";
 
 function MemoryEditForm(props) {
-  const { id, content, setShowEditForm, setMemories } = props;
+  const { id, event, content, image, setShowEditForm, setMemories } = props;
 
-  const [formContent, setFormContent] = useState(content);
+  const [memoryContent, setMemoryContent] = useState(content);
+  const [memoryImage, setMemoryImage] = useState(image);
+
+  const imageInput = useRef(null);
 
   const handleChange = (event) => {
-    setFormContent(event.target.value);
+    setMemoryContent(event.target.value);
+    console.log(memoryContent);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setMemoryImage(URL.createObjectURL(event.target.files[0]));
+      console.log(memoryImage);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("content", memoryContent);
+    if (imageInput.current.files.length) {
+      formData.append("image", imageInput.current?.files[0]);
+    }
+
     try {
-      await axiosRes.put(`/memories/${id}/`, {
-        content: formContent.trim(),
-      });
+      await axiosRes.put(`/memories/${id}/`, formData);
       setMemories((prevMemories) => ({
         ...prevMemories,
         results: prevMemories.results.map((memory) => {
           return memory.id === id
             ? {
                 ...memory,
-                content: formContent.trim(),
+                content: memoryContent.trim(),
+                image: memoryImage,
                 updated_at: "now",
               }
             : memory;
@@ -44,10 +66,47 @@ function MemoryEditForm(props) {
         <Form.Control
           className={styles.Form}
           as="textarea"
-          value={formContent}
+          name="content"
+          value={memoryContent}
           onChange={handleChange}
           rows={2}
         />
+      </Form.Group>
+      <Form.Group className="text-center">
+        {image ? (
+          <>
+            <figure>
+              <Image className={appStyles.Image} src={image} rounded />
+            </figure>
+            <div>
+              <Form.Label
+                className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                htmlFor="image-upload"
+              >
+                Add an image
+              </Form.Label>
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                required={false}
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </div>
+          </>
+        ) : (
+          <Form.Label
+            className="d-flex justify-content-center"
+            htmlFor="image-upload"
+          >
+            <img
+              src={Upload}
+              alt="Click or tap to upload an image"
+              className={styles.AssetImgMini}
+            />
+            {/* <Asset src={Upload} message="Click or tap to upload an image" /> */}
+          </Form.Label>
+        )}
       </Form.Group>
       <div className="text-right">
         <button
