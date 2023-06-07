26a168172d490314021ac95b476f7a62f8aa5e0f
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-07 11:44:14 +0200

    Update memory create form to include image upload

--- src/pages/memories/MemoryCreateForm.js
+++ src/pages/memories/MemoryCreateForm.js
@@ -1,28 +1,59 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/MemoryCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
+import Asset from "../../components/Asset";
+import { Alert, Image } from "react-bootstrap";
 
 function MemoryCreateForm(props) {
   const { event, setEvent, setMemories, profileImage, profile_id } = props;
-  const [content, setContent] = useState("");
+  const [memory, setMemory] = useState({
+    content: "",
+    image: "",
+    event: event,
+  });
+  const { content, image } = memory;
+  const [errors, setErrors] = useState({});
+
+  const imageInput = useRef(null);
 
   const handleChange = (event) => {
-    setContent(event.target.value);
+    setMemory({ ...memory, [event.target.name]: event.target.value });
+    console.log(memory);
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setMemory({
+        ...memory,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+      console.log(memory);
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("event", event);
+    formData.append("content", content);
+    if (imageInput.current.files.length) {
+      formData.append("image", imageInput.current?.files[0]);
+    }
+
     try {
-      const { data } = await axiosRes.post("/memories/", {
-        content,
-        event,
-      });
+      console.log(event);
+      const { data } = await axiosRes.post("/memories/", formData);
       setMemories((prevMemories) => ({
         ...prevMemories,
         results: [data, ...prevMemories.results],
@@ -35,7 +66,7 @@ function MemoryCreateForm(props) {
           },
         ],
       }));
-      setContent("");
+      setMemory("");
     } catch (err) {
       console.log(err);
     }
@@ -50,6 +81,7 @@ function MemoryCreateForm(props) {
           </Link>
           <Form.Control
             className={styles.Form}
+            name="content"
             placeholder="my memory..."
             as="textarea"
             value={content}
@@ -58,9 +90,53 @@ function MemoryCreateForm(props) {
           />
         </InputGroup>
       </Form.Group>
+
+      <Form.Group className="text-center">
+        {image ? (
+          <>
+            <figure>
+              <Image className={appStyles.Image} src={image} rounded />
+            </figure>
+            <div>
+              <Form.Label
+                className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                htmlFor="image-upload"
+              >
+                Add an image
+              </Form.Label>
+            </div>
+          </>
+        ) : (
+          <Form.Label
+            className="d-flex justify-content-center"
+            htmlFor="image-upload"
+          >
+            <img
+              src={Upload}
+              alt="Click or tap to upload an image"
+              className={styles.AssetImgMini}
+            />
+            {/* <Asset src={Upload} message="Click or tap to upload an image" /> */}
+          </Form.Label>
+        )}
+
+        <Form.File
+          id="image-upload"
+          accept="image/*"
+          required={false}
+          onChange={handleChangeImage}
+          ref={imageInput}
+        />
+      </Form.Group>
+      {errors?.image?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
       <button
         className={`${styles.Button} btn d-block ml-auto`}
-        disabled={!content.trim()}
+        // disabled={!memory.trim()}
         type="submit"
       >
         post
