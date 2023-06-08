db6793d6798a2a1c6a61bac0fa734395d100fdc0
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-08 16:02:38 +0200

    Update follow/unfollow to add/remove friend

--- src/pages/profiles/ProfilePageEvents.js
+++ src/pages/profiles/ProfilePageEvents.js
@@ -101,14 +101,14 @@ function ProfilePageEvents() {
                 className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
                 onClick={() => handleUnfollow(profile)}
               >
-                unfollow
+                remove friend
               </Button>
             ) : (
               <Button
                 className={`${btnStyles.Button} ${btnStyles.Black}`}
                 onClick={() => handleFollow(profile)}
               >
-                follow
+                add friend
               </Button>
             ))}
         </Col>
