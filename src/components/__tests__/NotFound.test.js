b25528a995976f28d47e70107bb98b4291fff0ac
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-13 18:20:43 +0200

    Add not found page test

---
+++ src/components/__tests__/NotFound.test.js
@@ -0,0 +1,15 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import NotFound from "../../pages/NotFound";
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
+
+test("NotFound page renders", () => {
+  render(
+    <Router>
+      <NotFound />
+    </Router>
+  );
+
+  const notFound = screen.getByText("Sorry, that page couldn't be found!");
+  expect(notFound).toBeInTheDocument();
+});
